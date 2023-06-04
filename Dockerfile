65c7c4fa4f3d3ab09cc1fe8f44c44c731ff73a80
Rachel <raalders28@gmail.com>
2023-06-04 09:44:44 +0200

    clean up

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:19
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-RUN npm run build
-CMD ["npm", "start"]
--- src/utils/sendtyping.ts
+++
@@ -1,7 +0,0 @@
-import { Channel } from 'discord.js';
-
-export async function sendTyping(channel: Channel) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  //@ts-ignore
-  await channel.sendTyping();
-}
