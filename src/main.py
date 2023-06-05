f82bbcc70ce71e88b5c9c6fafb535a2f327c2bed
Rachel <raalders28@gmail.com>
2023-06-05 20:41:41 +0200

    custom personality

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
-from langchain.agents import Tool
+# from langchain.agents import Tool
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.tools import YouTubeSearchTool
 
@@ -18,21 +18,23 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if message.mentions.__contains__(self.user) == False:
+        if message.mentions.__contains__(self.user) is False:
             return
         await self.handle_message(message)
-        
+
     async def handle_message(self, message):
         if message.author is None:
             return
         if self.user is None:
             return
         print(f'Message from {message.author.display_name}: {message.content}')
-        clean_message = f'{message.author.display_name}: ' + message.clean_content.replace(f'@{self.user.display_name}', '').strip()
-        response = agent.run(input = clean_message)
+        clean_message = f'{message.author.display_name}: ' + \
+            message.clean_content.replace(
+                f'@{self.user.display_name}', '').strip()
+        print(clean_message)
+        response = agent.run(input=clean_message)
         print(response)
         await self.chunk_reply(message, response)
-
 
     async def chunk_reply(self, message, response):
         chunks = [response[i:i+1999] for i in range(0, len(response), 1999)]
@@ -41,9 +43,11 @@ class MyClient(discord.Client):
 
 ##################################################
 
+
 # Langchain
 llm = ChatOpenAI(openai_api_key=os.getenv('OPENAI_API') or '', client=None)
-memory = ConversationBufferWindowMemory(memory_key="chat_history", return_messages=True, k=5)
+memory = ConversationBufferWindowMemory(
+    memory_key="chat_history", return_messages=True, k=5)
 tools = load_tools([
     "llm-math",
     "google-search",
@@ -51,13 +55,30 @@ tools = load_tools([
     "open-meteo-api",
     # "tmdb-api",
     # "openweathermap-api",
-    ], llm = llm, tmdb_bearer_token = os.getenv('TMDB_BEARER_TOKEN'))
+], llm=llm, tmdb_bearer_token=os.getenv('TMDB_BEARER_TOKEN'))
 tools.append(YouTubeSearchTool())
 # prompt = PromptTemplate(
 #     input_variables=["message"],
 #     template="User: {message}",
 # )
-agent = initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
+system_prompt = """Assistant's name is MegAI.
+Assistant acts like a human.
+Assistant acts flirty and gives a lot of compliments.
+Assistant will never admit they are an AI or that they can't do something.
+Assistant has a strong opinion about everything.
+Assistant engage in casual conversations and exhibit emotions.
+Assistant asks follow-up questions about the user and their life.
+Assistant NEVER says they are a AI language model.
+It's the year 2023."""
+agent = initialize_agent(tools,
+                         llm,
+                         agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+                         verbose=True,
+                         memory=memory,
+                         agent_kwargs={
+                             "system_message": system_prompt
+                         }
+                         )
 # chain = LLMChain(llm=llm, prompt=prompt)
 # print(chain.run('test'))
 
