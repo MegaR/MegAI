635025e92d7adc96377b758804d37dbd2ee07da2
Rachel <raalders28@gmail.com>
2023-06-27 09:13:55 +0200

    fix history manager

--- src/historymanager.ts
+++ src/historymanager.ts
@@ -4,7 +4,7 @@ export default class HistoryManager {
     messages: ChatCompletionRequestMessage[] = [];
     addMessage(message: ChatCompletionRequestMessage) {
         this.messages.push(message);
-        this.messages = this.messages.splice(0, 10);
+        this.messages = this.messages.slice(-10);
     }
     getHistory() {
         return this.messages;
