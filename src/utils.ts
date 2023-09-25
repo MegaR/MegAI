b114648b7564dd10d001465d66a7e3eed43db179
Rachel <raalders28@gmail.com>
2023-09-25 20:58:02 +0200

    bun lockfile

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+import { getLogger } from "./logger"
+
+const log = getLogger('utils');
+
+export async function retry<T>(func: () => Promise<T>, max: number): Promise<T> {
+    let lastError: any;
+    for(let i = 0; i < max; i++) {
+        try {
+            return await func();
+        } catch(e) {
+            lastError = e;
+            log.error(`Try ${i+1}`, e);
+        }
+    }
+    throw new Error(`Failed ${max} times`);
+}
