b7fa2186d268b414d98580139ed5e24f9f952d8f
Rachel <raalders28@gmail.com>
2023-10-14 13:53:27 +0200

    fix message sort order

--- src/commands/summary.command.ts
+++ src/commands/summary.command.ts
@@ -8,16 +8,17 @@ export const summaryCommand: Command<ChatInputCommandInteraction> = {
         .setDescription("Summarize the chat history"),
     handleCommand: async (_client: Client, interaction: ChatInputCommandInteraction) => {
         interaction.deferReply();
-        const messages = await interaction.channel?.messages.fetch({ limit: 50 });
+        const messages = await interaction.channel?.messages.fetch({ limit: 100 });
         if (messages === undefined) {
             await interaction.editReply("No messages found");
             return;
         }
         const question = `Please summerize the following chat history:\n` +
             messages.filter(message => !message.author.bot)
+                .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
                 .map(message => `${message.author.username}: ${message.cleanContent}`)
                 .join('\n');
-
+        
         const summary = await ai.chatCompletion([{ role: 'user', content: question }], { model: 'gpt-3.5-turbo-16k' });
         if(summary === undefined) {
             await interaction.editReply("❌ Summary failed");
