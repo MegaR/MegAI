d68ed28bd684852f34b922a3d4378a0df988a074
Rachel <raalders28@gmail.com>
2023-10-14 14:14:35 +0200

    ephemeral summary command

--- src/commands/summary.command.ts
+++ src/commands/summary.command.ts
@@ -1,19 +1,26 @@
 import { ChatInputCommandInteraction, Client, EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import Command from "./command.interface";
 import { ai } from "../openaiwrapper";
+import { getLogger } from "../logger";
+
+const log = getLogger("summaryCommand");
 
 export const summaryCommand: Command<ChatInputCommandInteraction> = {
     definition: new SlashCommandBuilder()
         .setName("summary")
         .setDescription("Summarize the chat history"),
+
     handleCommand: async (_client: Client, interaction: ChatInputCommandInteraction) => {
-        interaction.deferReply();
+        log.info(`Summary requested by ${interaction.user.username}`);
+
+        interaction.deferReply({ephemeral: true});
         const messages = await interaction.channel?.messages.fetch({ limit: 100 });
         if (messages === undefined) {
             await interaction.editReply("No messages found");
             return;
         }
-        const question = `Please summerize the following chat history:\n` +
+
+        const question = `Please give a detailed summary the following chat history. Use bulletpoints.\n` +
             messages.filter(message => !message.author.bot)
                 .sort((a, b) => a.createdTimestamp - b.createdTimestamp)
                 .map(message => `${message.author.username}: ${message.cleanContent}`)
