9363f0883b26b39478c9f83cc748ef24f98c5a1c
Rachel <raalders28@gmail.com>
2024-04-14 12:06:08 +0200

    walk to chest

--- src/minecraft/emptyinventory.mode.ts
+++ src/minecraft/emptyinventory.mode.ts
@@ -1,14 +1,23 @@
-import { Bot, EquipmentDestination } from "mineflayer";
+import { Bot } from "mineflayer";
 import BotMode from "./mode.interface";
 import { Block } from "prismarine-block";
 import idleMode from "./idle.mode";
 import { getLogger } from "../logger";
+import { Movements, goals } from "mineflayer-pathfinder";
 
 const logger = getLogger("emptyInventoryMode");
 export default function emptyInventoryMode(bot: Bot, block: Block): BotMode {
     return {
         name: "Empty inventory",
         start: async () => {
+            const movement = new Movements(bot);
+            movement.canOpenDoors = true;
+            movement.canDig = false;
+            movement.allowParkour = true;
+            movement.allowFreeMotion = true;
+            bot.pathfinder.setMovements(movement);
+            await bot.pathfinder.goto(new goals.GoalNear(block.position.x, block.position.y, block.position.z, 4));
+
             if(bot.inventory.emptySlotCount() >= 5) {
                 const equipment = bot.entity.equipment;
                 if(equipment[1]) {
